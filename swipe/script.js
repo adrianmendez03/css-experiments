36ef256b60447190fceede8ab7439be12864fdda
Adrian Mendez <adrianmndz328@gmail.com>
2021-05-17 16:52:33 -0500

    infinite loop achieved

--- swipe/script.js
+++ swipe/script.js
@@ -1,9 +1,17 @@
 $(() => {
+
+    function loop (li) {
+        li.css({ left: '-150px' })
+        li.animate({ left: '100%' }, 10000, 'linear', () => {
+            loop(li)
+        })
+    }
+
     const lis = $("li")
     let time = 0
     for (let i = 0; i < lis.length; i++) {
         setTimeout(() => {
-            lis.eq(i).animate({ left: '100%' }, 10000,'linear')
+            loop(lis.eq(i))
         }, time)
         time += 2000
     }
